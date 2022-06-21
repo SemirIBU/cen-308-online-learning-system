a31ebee6dc107840892cbd92396b6eaf94408819
Admir Šahman <admir.sahman@waltercode.com>
2022-06-21 11:13:02 +0200

    production server url update

--- api/routes/api-doc.php
+++ api/routes/api-doc.php
@@ -5,7 +5,7 @@
  * @OA\Info(title="Teach me API", version="0.2")
  * @OA\OpenApi(
  *   @OA\Server(url="http://mylocalhost.com/cen-308-online-learning-system/api/", description="Development environment"),
- *   @OA\Server(url="https://clownfish-app-qv4qs.ondigitalocean.app/api/", description="Production environment")
+ *   @OA\Server(url="https://admir.software/api/", description="Production environment")
  * ),
  * @OA\SecurityScheme(securityScheme="ApiKeyAuth", type="apiKey", in="header", name="Authentication" )
  */
