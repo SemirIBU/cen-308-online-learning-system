efbbcaff450ab4dd8eca49013194d78db279c9bf
Admir Šahman <admir.sahman@waltercode.com>
2022-06-23 21:58:01 +0200

    added quiz tables to db

--- db-dump.sql
+++ db-dump.sql
@@ -31,6 +31,16 @@ CREATE TABLE `accounts` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `accounts`
+--
+
+LOCK TABLES `accounts` WRITE;
+/*!40000 ALTER TABLE `accounts` DISABLE KEYS */;
+INSERT INTO `accounts` VALUES (1161,'2022-06-01 14:38:53','ACTIVE'),(1162,'2022-06-01 18:19:32','PENDING'),(1163,'2022-06-03 17:39:58','ACTIVE'),(1164,'2022-06-21 11:10:43','ACTIVE'),(1165,'2022-06-22 09:10:22','PENDING'),(1166,'2022-06-22 09:10:43','PENDING'),(1167,'2022-06-22 09:11:59','PENDING'),(1168,'2022-06-22 09:22:54','PENDING');
+/*!40000 ALTER TABLE `accounts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `courses`
 --
 
@@ -47,8 +57,18 @@ CREATE TABLE `courses` (
   UNIQUE KEY `uq_course_name` (`name`),
   KEY `fk_course_account_idx` (`account_id`),
   CONSTRAINT `fk_course_account` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=59 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=61 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `courses`
+--
+
+LOCK TABLES `courses` WRITE;
+/*!40000 ALTER TABLE `courses` DISABLE KEYS */;
+INSERT INTO `courses` VALUES (48,'asfafadgadga','adfadf','active',1161),(49,'asdafsagadgga','sadas','active',1161),(50,'name','description','active',1161),(51,'asfasfasfasf','asfasfasfas','active',1161),(52,'safasfasf','asfafas','active',1161),(53,'sadasdsaasda','asdasdda','active',1161),(54,'cascascascas','cascsac','active',1161),(55,'asdsadsda','asdasda','active',1161),(58,'1fasfa updateasdsada','asdsa','active',1161),(59,'1fasfa update','dasdsad','active',1161),(60,'hdhghd','hgdhgddgh','active',1161);
+/*!40000 ALTER TABLE `courses` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `professors`
@@ -73,6 +93,125 @@ CREATE TABLE `professors` (
   CONSTRAINT `fk_professor_account` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `professors`
+--
+
+LOCK TABLES `professors` WRITE;
+/*!40000 ALTER TABLE `professors` DISABLE KEYS */;
+/*!40000 ALTER TABLE `professors` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `question_choices`
+--
+
+DROP TABLE IF EXISTS `question_choices`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `question_choices` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `question_id` int unsigned NOT NULL,
+  `is_right_choice` enum('0','1') COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `choice` varchar(150) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_question_id` FOREIGN KEY (`id`) REFERENCES `questions` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `question_choices`
+--
+
+LOCK TABLES `question_choices` WRITE;
+/*!40000 ALTER TABLE `question_choices` DISABLE KEYS */;
+/*!40000 ALTER TABLE `question_choices` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `questions`
+--
+
+DROP TABLE IF EXISTS `questions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `questions` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `question` mediumtext COLLATE utf8mb4_unicode_ci,
+  `is_active` enum('0','1') COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `questions`
+--
+
+LOCK TABLES `questions` WRITE;
+/*!40000 ALTER TABLE `questions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `questions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `student_course`
+--
+
+DROP TABLE IF EXISTS `student_course`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `student_course` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `student_id` int unsigned NOT NULL,
+  `course_id` int unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_user_course_idx` (`course_id`),
+  KEY `fk_course_user_idx` (`student_id`),
+  CONSTRAINT `fk_course_student` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_student_course` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `student_course`
+--
+
+LOCK TABLES `student_course` WRITE;
+/*!40000 ALTER TABLE `student_course` DISABLE KEYS */;
+INSERT INTO `student_course` VALUES (2,158,49),(3,158,54),(10,160,58),(16,160,50),(17,160,51),(18,160,59);
+/*!40000 ALTER TABLE `student_course` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `student_question_answers`
+--
+
+DROP TABLE IF EXISTS `student_question_answers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `student_question_answers` (
+  `student_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `question_id` int unsigned NOT NULL,
+  `choice_id` int unsigned NOT NULL,
+  `is_right` enum('0','1') COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `answer_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`student_id`),
+  KEY `fk_student_choice_idx` (`choice_id`),
+  KEY `fk_student_question_idx` (`question_id`),
+  CONSTRAINT `fk_student_choice` FOREIGN KEY (`choice_id`) REFERENCES `question_choices` (`id`),
+  CONSTRAINT `fk_student_question` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`),
+  CONSTRAINT `fk_student_question_student` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `student_question_answers`
+--
+
+LOCK TABLES `student_question_answers` WRITE;
+/*!40000 ALTER TABLE `student_question_answers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `student_question_answers` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `students`
@@ -105,6 +244,16 @@ CREATE TABLE `students` (
   CONSTRAINT `fk_user_account` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=163 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `students`
+--
+
+LOCK TABLES `students` WRITE;
+/*!40000 ALTER TABLE `students` DISABLE KEYS */;
+INSERT INTO `students` VALUES (158,'Admir','Šahman','admir.sahman@stu.ibu.edu.ba','+38762616940','827ccb0eea8a706c4c34a16891f84e7b','BiH','Sarajevo','71210','Podigmanska 4','ACTIVE','2022-06-01 14:38:53','admin','c93d51ea02f5488eff12a47e15c8b7f3','2022-06-21 09:49:25',1161),(159,'First Last name','','myemail2@gmail.com','+387999999','827ccb0eea8a706c4c34a16891f84e7b','BiH','Sarajevo','71200','Francuske revolucije','PENDING','2022-06-01 18:19:32','student','8a79d3153496aa6259bd341484474936',NULL,1162),(160,'sdasdas','','admir.sahman+1@stu.ibu.edu.ba',NULL,'827ccb0eea8a706c4c34a16891f84e7b',NULL,NULL,NULL,NULL,'ACTIVE','2022-06-21 11:10:43','student',NULL,NULL,1164),(161,'1fasfa update','','admir.sahman+5@stu.ibu.edu.ba','+38762616940','827ccb0eea8a706c4c34a16891f84e7b','BiH','Sarajevo','71210','podigmanska 4','PENDING','2022-06-22 09:11:59','student','bd73ab637f0b724cef9fafd24ea00b42',NULL,1167),(162,'test','testic','admir.sahman+33@stu.ibu.edu.ba','+38762616940','827ccb0eea8a706c4c34a16891f84e7b','BiH','Sarajevo','71210','podigmanska 4','PENDING','2022-06-22 09:22:54','student','bdc60653e0fdd7ef34ebbd5723f2b47e',NULL,1168);
+/*!40000 ALTER TABLE `students` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -115,4 +264,4 @@ CREATE TABLE `students` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-06-22 13:41:36
+-- Dump completed on 2022-06-23 21:55:46
